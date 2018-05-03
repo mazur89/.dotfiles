404b6053b84481faea6415c9728c6dfbfb0ed25c
Przemek <mazur89@gmail.com>
2018-05-03 16:34:28 +0100

    oh-my-zsh

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@ ln -sf ~/.dotfiles/bashrc ~/.bashrc
 
 # zsh
 sudo apt-get -y install zsh
+rm -rf ~/.zshrc
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
+rm -rf ~/.oh-my-zsh
+ln -sf ~/.dotfiles/oh-my-zsh ~/.oh-my-zsh
