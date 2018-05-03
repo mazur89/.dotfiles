be7ccbcf8772827832bc87109cf09da088966228
Przemek <mazur89@gmail.com>
2018-05-03 16:12:04 +0100

    yes

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 ln -sf ~/.dotfiles/bashrc ~/.bashrc
 
 # zsh
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install zsh
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install zsh
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
