2520842f49dbb17591fa4a7ddb0e836a6de3ba67
przemek <mazur89@gmail.com>
2018-06-04 18:33:47 +0100

    emacs

---
+++ emacs.d/elpa/dash-20180413.30/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("dash.el") (23317 30650 332313 708000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
